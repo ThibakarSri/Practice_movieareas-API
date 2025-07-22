e1f39f95df3108ddb721e58733a152c479fa880d
ThibakarSri <thibakarsri33@gmail.com>
2025-07-22 21:01:30 +0530

    route structures

--- main.js
+++ main.js
@@ -9,9 +9,36 @@ app.get('/', (req, res) => {
     res.json({ msg: "Hello students!" });
 });
 
+// CRUD functionality of movies
+
+// R - For Reading
+app.get('/movies', () => {
+
+})
+
+// C - For creating movies
+app.post('/movies', () => {
+    
+})
+
+// U - For pdating movie
+app.put('./movies/:id', () => {
+
+})
+
+// D - For deleting movie
+app.delete('./movies/:id', () => {
+
+})
+
+
 app.listen(PORT, () => {
     console.log(`The server is running at http://localhost:${PORT}`);
 });
+
+
+
+
 
 // DRY Principle
 // KISS Principle (Keep It Simple, Stupid!)
